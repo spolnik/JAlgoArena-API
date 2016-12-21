b48090160e30e46b6373ea05a2a26d6b88c33210
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-21 18:59:29 +0100

    - enable cors

--- src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
+++ src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
@@ -4,11 +4,37 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.filter.CorsFilter
+
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-open class JAlgoArenaApiGatewayApplication
+open class JAlgoArenaApiGatewayApplication {
+
+    @Bean
+    open fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("OPTIONS")
+        config.addAllowedMethod("HEAD")
+        config.addAllowedMethod("GET")
+        config.addAllowedMethod("PUT")
+        config.addAllowedMethod("POST")
+        config.addAllowedMethod("DELETE")
+        config.addAllowedMethod("PATCH")
+        source.registerCorsConfiguration("/**", config)
+
+        return CorsFilter(source)
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(JAlgoArenaApiGatewayApplication::class.java, *args)
