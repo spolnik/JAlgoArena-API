32265f0af37b47c559b79c11fc6a73f821a40f02
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-12-25 16:42:59 +0100

    - add logging of routes

--- src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
+++ src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
@@ -1,15 +1,33 @@
 package com.jalgoarena
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean
+import org.springframework.boot.CommandLineRunner
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator
 
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-open class JAlgoArenaApiGatewayApplication
+@Configuration
+open class JAlgoArenaApiGatewayApplication {
+
+    private val LOG = LoggerFactory.getLogger(this.javaClass)
+
+    @Bean
+    open fun commandLineRunner(routeLocator: RouteLocator): CommandLineRunner =
+            CommandLineRunner { args ->
+                routeLocator.routes
+                        .forEach { route ->
+                            LOG.info("${route.id} (${route.location}) ${route.fullPath}")
+                        }
+            }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(JAlgoArenaApiGatewayApplication::class.java, *args)
