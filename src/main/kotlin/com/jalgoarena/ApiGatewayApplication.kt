b9c10904ba6db6a6434b1f39c172d246ecf1d87c
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-28 19:27:32 +0100

    - new feature - add centralized support for CORS

--- src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
+++ src/main/kotlin/com/jalgoarena/ApiGatewayApplication.kt
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Bean
 import org.springframework.boot.CommandLineRunner
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 
 @SpringBootApplication
@@ -27,6 +30,24 @@ open class JAlgoArenaApiGatewayApplication {
                             LOG.info("${route.id} (${route.location}) ${route.fullPath}")
                         }
             }
+
+    @Bean
+    open fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", CorsConfiguration().apply {
+            allowCredentials = true
+            addAllowedOrigin("*")
+            addAllowedHeader("*")
+            addAllowedMethod("OPTIONS")
+            addAllowedMethod("HEAD")
+            addAllowedMethod("GET")
+            addAllowedMethod("PUT")
+            addAllowedMethod("POST")
+            addAllowedMethod("DELETE")
+            addAllowedMethod("PATCH")
+        })
+        return CorsFilter(source)
+    }
 }
 
 fun main(args: Array<String>) {
