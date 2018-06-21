117d6b5f7824809ee877bb0b9886c2fcf3a1eb14
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-06-21 06:52:25 +0200

    - update pm2 configs (remove eureka, update version)

--- pm2.config.js
+++ pm2.config.js
@@ -4,12 +4,11 @@ module.exports = {
             name: 'api',
             args: [
                 "-jar",
-                "build/libs/jalgoarena-api-2.0.0-SNAPSHOT.jar"
+                "build/libs/jalgoarena-api-2.1.0-SNAPSHOT.jar"
             ],
             script: 'java',
             env: {
-                PORT: 5001,
-                EUREKA_URL: 'http://localhost:5000/eureka/'
+                PORT: 5001
             }
         }
     ]
